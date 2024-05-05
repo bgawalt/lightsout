799159a19bd5faf8e788bf454bba0359b9a548f2
Brian Gawalt <bgawalt@gmail.com>
2024-05-04 21:47:33 -0700

    Upgrade LightsOut class to frozen dataclass

--- lightsout.py
+++ lightsout.py
@@ -1,35 +1,38 @@
 """Represent game state for Lights Out."""
 
+import dataclasses
 
+
+@dataclasses.dataclass(frozen=True)
 class LightsOut:
+    size: int
+    state: int = 0
 
-    def __init__(self, size: int):
-        if size <= 0:
-            raise ValueError(f"size must be positive; not {size}")
-        self._size = size
-        # Bitmask representing a binary matrix in row-major format.
-        self._state = 0
+    def __post_init__(self):
+        if self.size <= 0:
+            raise ValueError(f"size must be positive; not {self.size}")
+        if self.state < 0:
+            raise ValueError(f"state must be non-negative; not {self.state}")
+        if self.state >= (2 ** (self.size * self.size)):
+            raise ValueError(f"state exceeds bounds for size {self.size}: ")
 
     def solved(self) -> bool:
-        return self._state == 0
-    
-    def state(self) -> int:
-        return self._state
+        return self.state == 0
 
     def _valid_pos(self, row: int, col: int) -> bool:
-        return all(0 <= dim < self._size for dim in (row, col))
+        return all(0 <= dim < self.size for dim in (row, col))
 
-    def toggle(self, row: int, col: int):
+    def toggle(self, row: int, col: int) -> 'LightsOut':
         if not self._valid_pos(row, col):
             raise ValueError(
-                f"Invalid position for grid size {self._size}: ({row}, {col}")
-        mask = 2 ** (self._size * row + col)
+                f"Invalid position for grid size {self.size}: ({row}, {col}")
+        mask = 2 ** (self.size * row + col)
         neighbors = [
             (row, col + 1), (row, col - 1), (row - 1, col), (row + 1, col)
         ]
         for n_row, n_col in neighbors:
             if not self._valid_pos(n_row, n_col):
                 continue
-            mask += 2 ** (self._size * n_row + n_col)
-        self._state ^= mask
+            mask += 2 ** (self.size * n_row + n_col)
+        return dataclasses.replace(self, state=(self.state ^ mask))
         
--- lightsout_test.py
+++ lightsout_test.py
@@ -10,8 +10,8 @@ def test_init_solved():
 
 def test_init_state():
     game = lightsout.LightsOut(5)
-    assert game.state() == 0, f"\
-        initial games should have state 0, not {game.state()}"
+    assert game.state == 0, f"\
+        initial games should have state 0, not {game.state}"
 
 
 def test_toggle_bad_pos():
@@ -36,19 +36,19 @@ def test_toggle_repeat():
     game = lightsout.LightsOut(5)
     for row in range(5):
         for col in range(5):
-            game.toggle(row, col)
-            assert game.state() != 0, f"\
+            game = game.toggle(row, col)
+            assert game.state != 0, f"\
                 Toggle at ({row}, {col}) produced a zero state"
-            game.toggle(row, col)
-            assert game.state() == 0, f"\
-                Repeat of ({row}, {col}) yielded non-zero state: {game.state()}"
+            game = game.toggle(row, col)
+            assert game.state == 0, f"\
+                Repeat of ({row}, {col}) yielded non-zero state: {game.state}"
 
 
 def test_state_size_one():
     game = lightsout.LightsOut(1)
-    game.toggle(0, 0)
-    assert game.state() == 1, f"\
-        Toggle at (0, 0) produced state {game.state()}, not 1"
+    next = game.toggle(0, 0)
+    assert next.state == 1, f"\
+        Toggle at (0, 0) produced state {next.state}, not 1"
 
 
 def test_state_size_two():
@@ -56,30 +56,30 @@ def test_state_size_two():
     # Grid: 
     #   0 0
     #   0 0
-    game.toggle(0, 0)
+    game = game.toggle(0, 0)
     # Grid: 
     #   1 1
     #   1 0
-    assert game.state() == 7, f"\
-        Toggle at (0, 0) produced state {game.state()}, not 7"
-    game.toggle(1, 1)
+    assert game.state == 7, f"\
+        Toggle at (0, 0) produced state {game.state}, not 7"
+    game = game.toggle(1, 1)
     # Grid: 
     #   1 0
     #   0 1
-    assert game.state() == 9, f"\
-        Toggle at (1, 1) produced state {game.state()}, not 9"
-    game.toggle(0, 1)
+    assert game.state == 9, f"\
+        Toggle at (1, 1) produced state {game.state}, not 9"
+    game = game.toggle(0, 1)
     # Grid: 
     #   0 1
     #   0 0
-    assert game.state() == 2, f"\
-        Toggle at (1, 1) produced state {game.state()}, not 2"
-    game.toggle(1, 0)
+    assert game.state == 2, f"\
+        Toggle at (1, 1) produced state {game.state}, not 2"
+    game = game.toggle(1, 0)
     # Grid: 
     #   1 1
     #   1 1
-    assert game.state() == 15, f"\
-        Toggle at (1, 1) produced state {game.state()}, not 15"
+    assert game.state == 15, f"\
+        Toggle at (1, 1) produced state {game.state}, not 15"
     
 
 def main():
